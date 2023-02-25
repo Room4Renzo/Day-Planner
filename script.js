d919202a2fe7c24a5ede4e2ae1c7790b43f0f8c2
Room4Renzo <lorenzo.afw@gmail.com>
2023-02-25 15:30:53 +0000

    modified the for loop to change colours based on time of day

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ const currentDay = $(`#currentDay`);
 let today = moment().format(`dddd, MMMM Do`);
 let todayIs = (`Today is: ` + today);
 currentDay.text(todayIs);
+let currentTime = moment().hour()
+console.log('currentTime: ', currentTime);
+const time = $(`.time-block`);
+const description = $(`.description`);
 
-
+// for loop compares id of all items of block class against current time
+for (let i = 0; i < 9; i++) {
+    if (currentTime === time.attr(`id`)) {
+        description.attr(`class`, `present`)
+    } else if (currentTime > time.attr(`id`)) {
+        description = $(`class`, `past`)
+    } else if (currentTime < time.attr(`id`)) {
+        description = $(`class`, `future`)
+    }
+}
