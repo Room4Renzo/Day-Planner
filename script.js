aab5af4a70fc7622fcf4ef0e8ded2b8f635ebfa1
Room4Renzo <lorenzo.afw@gmail.com>
2023-03-06 15:59:26 +0000

    Changed id to 24 hour clock to be able to use moment().hour() to store and tretrieve from local storage correctly and display timblocks with correct colours

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ const today = moment().format(`dddd, MMMM Do`);
 const todayIs = (`Today is: ` + today);
 currentDay.text(todayIs);
 const currentTime = moment().hour()
-console.log('currentTime: ', currentTime);
 const time = $(`.time-block`);
 const description = $(`.description`);
 const saveBtn = $(`.saveBtn`);
 
-
+// Conditional to compare time and add style classes
 description.each(function () {
-    if (currentTime === ($(this).parent().attr(`id`))) {
+    if (currentTime == ($(this).parent().attr(`id`))) {
         $(this).removeClass(`past`);
         $(this).removeClass(`future`);
         $(this).addClass(`present`);
@@ -28,18 +27,20 @@ description.each(function () {
     };
 });
 
+// event listener to store input to local storage
 saveBtn.on(`click`, function () {
     let text = $(this).siblings(`.description`).val();
     let time = $(this).parent().attr(`id`);
     localStorage.setItem(time, text);
 });
 
+// Retrieves data from local storage and displays
 $('#9 .description').val(localStorage.getItem('9'));
 $('#10 .description').val(localStorage.getItem('10'));
 $('#11 .description').val(localStorage.getItem('11'));
 $('#12 .description').val(localStorage.getItem('12'));
-$('#1 .description').val(localStorage.getItem('1'));
-$('#2 .description').val(localStorage.getItem('2'));
-$('#3 .description').val(localStorage.getItem('3'));
-$('#4 .description').val(localStorage.getItem('4'));
-$('#5 .description').val(localStorage.getItem('5'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16'));
+$('#17 .description').val(localStorage.getItem('17'));
