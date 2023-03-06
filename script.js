62c3b620cc2eb2542790a9ffd5704ecbc86a3a07
Room4Renzo <lorenzo.afw@gmail.com>
2023-03-06 15:30:32 +0000

    managed to retrieve and display correctly from localstorage. trying to get moment hour to register correctly but lost on syntax

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 const currentDay = $(`#currentDay`);
-let today = moment().format(`dddd, MMMM Do`);
-let todayIs = (`Today is: ` + today);
+const today = moment().format(`dddd, MMMM Do`);
+const todayIs = (`Today is: ` + today);
 currentDay.text(todayIs);
-let currentTime = moment().hour()
+const currentTime = moment().hour()
 console.log('currentTime: ', currentTime);
 const time = $(`.time-block`);
 const description = $(`.description`);
 const saveBtn = $(`.saveBtn`);
 
 
-description.each(function() {
-    if (currentTime === ($(this).parent().attr(`id`))){
+description.each(function () {
+    if (currentTime === ($(this).parent().attr(`id`))) {
+        $(this).removeClass(`past`);
+        $(this).removeClass(`future`);
         $(this).addClass(`present`);
-    } else if (currentTime > ($(this).parent().attr(`id`))){
+
+
+    } else if (currentTime > ($(this).parent().attr(`id`))) {
+        $(this).removeClass(`present`);
+        $(this).removeClass(`future`);
         $(this).addClass(`past`);
-        } else if (currentTime < ($(this).parent().attr(`id`))){
-            $(this).addClass(`future`);
-        };
-    });
 
-    saveBtn.on(`click`, function(){
-        let text = $(this).siblings(`.description`).val();
-        let time = $(this).parent().attr(`id`);
-        localStorage.setItem(time, text);
-    });
+    } else if (currentTime < ($(this).parent().attr(`id`))) {
+        $(this).removeClass(`past`);
+        $(this).removeClass(`present`);
+        $(this).addClass(`future`);
+    };
+});
 
-    $(this).siblings(`description`).val(localStorage.getItem(time, text))
+saveBtn.on(`click`, function () {
+    let text = $(this).siblings(`.description`).val();
+    let time = $(this).parent().attr(`id`);
+    localStorage.setItem(time, text);
+});
+
+$('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#1 .description').val(localStorage.getItem('1'));
+$('#2 .description').val(localStorage.getItem('2'));
+$('#3 .description').val(localStorage.getItem('3'));
+$('#4 .description').val(localStorage.getItem('4'));
+$('#5 .description').val(localStorage.getItem('5'));
